66d2d5af9548682c1baffd655aea0fcd1da0e924
tevz321 <tevz.stergar@inel.com>
2025-06-19 13:34:59 +0200

    Add as WindowsService

--- GtinSolution/GtinApi/Program.cs
+++ GtinSolution/GtinApi/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Host.UseWindowsService();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularApp", policy =>
